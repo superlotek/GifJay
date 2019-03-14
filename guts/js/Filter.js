fd264f7890f007e4cc93684b38c9036de37a73d1
Ron Husges <ron.husges@internetbrands.com>
2019-03-14 13:03:37 -0700

    Rebuild the Filters Object

--- guts/js/Filter.js
+++ guts/js/Filter.js
@@ -2,51 +2,131 @@
 // *** FILTERS ***
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+var filters = {
+  "stage" : [
+    {"saturation": "blurp", "hueshift": "bleek"},
+    {"saturation": "block", "hueshift": "gleep"}
+  ],
+  "string": ""
+}
+
+/* hueShift 0deg
+  SATURATE 100%
+  invert 0%
+  blur 0%
+
+  name: 'grayscale',
+  default: 0,
+  value: 0,
+  min: 0,
+  max: 100,
+  unit: '%'
+*/
+
+var saturateDefault = 1;
+
 const Filter = {
 
-  addFilter(filterNum, filterString) {
-    filtersOnString = "";
-    filtersOn[filterNum] = filterString;
-    filtersOn.forEach(function(element) {
+  // addFilter(filterNum, filterString) {
+  addFilter() {
+    // filtersOnString = "";
+    // filtersOn[filterNum] = filterString;
+    filters.stage.forEach(function(element) {
       filtersOnString += element + " ";
     });
+
+    console.log(filtersOnString);
+    // console.log(filters.stage[0].string);
+    // console.log(filters.stage[1].string);
+
   },
+
+
+/*
+
+
+[
+  {name: "Joe", age: 22},
+  {name: "Kevin", age: 24},
+  {name: "Peter", age: 21}
+].map(function(elem){
+    return elem.name;
+}).join(",");
+
+*/
+
 
   // FILTER FX : SATURATE
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   saturator() {
-    s1SaturatorValue = numRan(saturateAmount);
-    s2SaturatorValue = numRan(saturateAmount);
-    s1SaturateString = "saturate(" + s1SaturatorValue + ")";
-    s2SaturateString = "saturate(" + s2SaturatorValue + ")";
-    if (stgSelect == "all") {
-      $(s1).css('-webkit-filter', s1SaturateString);
-      $(s2).css('-webkit-filter', s2SaturateString);
-      this.addFilter(0, s2SaturateString);
-      $(s1).add(s2).css('-webkit-filter', filtersOnString);
+
+    // filters.stage[0].saturation = "saturate(" + numRan(saturateAmount) + ")";
+    // filters.stage[1].saturation = "saturate(" + numRan(saturateAmount) + ")";
+    // $(s1).css('-webkit-filter', filters.stage[0].saturation);
+    // $(s2).css('-webkit-filter', filters.stage[1].saturation);
+
+    if (stgSelect == s1) {
+      console.log("SATURATE: S1");
+      filters.stage[0].saturation = "saturate(" + numRan(saturateAmount) + ")";
+      filters.stage[1].saturation = "saturate(" + saturateDefault + ")";
+    } else if (stgSelect == s2) {
+      console.log("SATURATE: S2");
+      filters.stage[0].saturation = "saturate(" + saturateDefault + ")";
+      filters.stage[1].saturation = "saturate(" + numRan(saturateAmount) + ")";
     } else {
-      this.addFilter(0, s1SaturateString);
-      $(stgSelect).css('-webkit-filter', filtersOnString);
+      console.log("SATURATE: S1+S2");
+      filters.stage[0].saturation = "saturate(" + numRan(saturateAmount) + ")";
+      filters.stage[1].saturation = "saturate(" + numRan(saturateAmount) + ")";
     }
+
+    $(s1).css('-webkit-filter', filters.stage[0].saturation);
+    $(s2).css('-webkit-filter', filters.stage[1].saturation);
+
+    filters.stage[0].string = filters.stage[0].saturation;
+    filters.stage[1].string = filters.stage[1].saturation;
+
+    // this.addFilter()
+    //   this.addFilter(0, s1SaturateString);
   },
 
   // FILTER FX : HUESHIFT
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   hueShift() {
-    s1HueshiftValue = numRan(360);
-    s2HueshiftValue = numRan(360);
-    s1HueshiftString = "hue-rotate(" + s1HueshiftValue + "deg)";
-    s2HueshiftString = "hue-rotate(" + s2HueshiftValue + "deg)";
-    if (stgSelect == "all") {
-      console.log('FX HUESHIFT: STG 1+2');
-      $(s1).css('-webkit-filter', s1HueshiftString);
-      $(s2).css('-webkit-filter', s2HueshiftString);
-      this.addFilter(1, s2HueshiftString)
-      $(s1).add(s2).css('-webkit-filter', filtersOnString);
+
+
+    if (stgSelect == s1) {
+      console.log("HUESHIFT: S1");
+      filters.stage[0].hueshift = "hue-rotate(" + numRan(360) + "deg)";
+      filters.stage[1].hueshift = "hue-rotate(" + numRan(0) + "deg)";
+    } else if (stgSelect == s2) {
+      console.log("HUESHIFT: S2");
+      filters.stage[0].hueshift = "hue-rotate(" + numRan(0) + "deg)";
+      filters.stage[1].hueshift = "hue-rotate(" + numRan(360) + "deg)";
     } else {
-      this.addFilter(1, s1HueshiftString)
-      $(stgSelect).css('-webkit-filter', filtersOnString);
+      console.log("HUESHIFT: S1+S2");
+      filters.stage[0].hueshift = "hue-rotate(" + numRan(360) + "deg)";
+      filters.stage[1].hueshift = "hue-rotate(" + numRan(360) + "deg)";
     }
+
+    $(s1).css('-webkit-filter', filters.stage[0].hueshift);
+    $(s2).css('-webkit-filter', filters.stage[1].hueshift);
+
+
+
+    // s1HueshiftValue = numRan(360);
+    // s2HueshiftValue = numRan(360);
+    // s1HueshiftString = "hue-rotate(" + s1HueshiftValue + "deg)";
+    // s2HueshiftString = "hue-rotate(" + s2HueshiftValue + "deg)";
+    // if (stgSelect == "all") {
+    //   console.log('FX HUESHIFT: STG 1+2');
+    //   $(s1).css('-webkit-filter', s1HueshiftString);
+    //   $(s2).css('-webkit-filter', s2HueshiftString);
+    //   this.addFilter(1, s2HueshiftString)
+    //   $(s1).add(s2).css('-webkit-filter', filtersOnString);
+    // } else {
+    //   this.addFilter(1, s1HueshiftString)
+    //   $(stgSelect).css('-webkit-filter', filtersOnString);
+    // }
   },
 
   // FILTER FX : BLURRY
